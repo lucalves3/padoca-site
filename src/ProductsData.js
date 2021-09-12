5875df513f44dd641629b159d62527e13586b083
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-12 18:39:45 -0300

    cardapio data add

---
+++ src/ProductsData.js
@@ -0,0 +1,68 @@
+const esfirras = [
+  {
+    id: 'Maionese Adicional 40g',
+    Text: 'Maionese da Padoca, feita sem ingredientes de origem animal, sem conservantes e sem industrializados.',
+    Acompanhamento: 'Acompanha maionese da casa',
+    Ingredientes: 'Igredientes massa: Farinha de trigo, farinha de trigointegral, água, óleo de girassol, açucar, sal e fermento biológico.',
+    Aviso: 'Alérgicos e intolerantes: Todos os nossos preparos cotém glúten,portanto não são indicados para celíacos. Sem ingredientes de origemanimal',
+    Tag: 'Sem ingredientes de origem animal',
+    Price: 'R$: 6,00',
+    SourceImage: 'https://cdn.neemo.com.br/uploads/item/photo/1011894/logo_WhatsApp_Image_2021-06-23_at_21.58.45.jpeg',
+  },
+  {
+    id: 'Soja',
+    Text: 'Proteína de soja defumada bem temperada e azeitona.',
+    Acompanhamento: 'Acompanha maionese da casa',
+    Ingredientes: 'Igredientes massa: Farinha de trigo, farinha de trigo integral, água, óleo de girassol, açucar, sal e fermento biológico.',
+    Aviso: 'Alérgicos e intolerantes: Todos os nossos preparos cotém glúten, portanto não são indicados para celíacos. Sem ingredientes de origem animal',
+    Tag: 'Sem ingredientes de origem animal',
+    Price: 'R$: 6,00',
+    SourceImage: 'https://cdn.neemo.com.br/uploads/item/photo/987112/logo_soja.JPG',
+  },
+  {
+    id: 'Marguerita',
+    Text: 'Queijo de castanha de caju, tomate fresco, orégano e manjericão fresco',
+    Acompanhamento: 'Acompanha maionese da casa',
+    Ingredientes: 'Igredientes massa: Farinha de trigo, farinha de trigo integral, água, óleo de girassol, açucar, sal e fermento biológico.',
+    Aviso: 'Alérgicos e intolerantes: Todos os nossos preparos cotém glúten, portanto não são indicados para celíacos.',
+    Tag: 'Sem ingredientes de origem animal',
+    Price: 'R$: 6,00',
+    SourceImage: 'https://cdn.neemo.com.br/uploads/item/photo/987111/logo_esfirra_marguerita.JPG',
+  },
+  {
+    id: 'Cheddar com Cebola Caramelizada',
+    Text: 'Cheddar artesanal com cebola caramelizada',
+    Acompanhamento: 'Acompanha maionese da casa',
+    Ingredientes: 'Igredientes massa: Farinha de trigo, farinha de trigo integral, água, óleo de girassol, açucar, sal e fermento biológico.',
+    Aviso: 'Alérgicos e intolerantes: Todos os nossos preparos cotém glúten, portanto não são indicados para celíacos. Sem ingredientes de origem animal',
+    Tag: 'Sem ingredientes de origem animal',
+    Price: 'R$: 6,00',
+    SourceImage: 'https://cdn.neemo.com.br/uploads/item/photo/987113/logo_cheddar.JPG',
+  },
+  {
+    id: 'kit 6 esfirras',
+    Text: 'Kit de 6 esfirras com sabores variados. Aqui você pode escolher esse delicioso Kit com recheios variados. Acompanha maionese da Padoca.',
+    Acompanhamento: 'Acompanha maionese da casa',
+    Ingredientes: 'Igredientes massa: Farinha de trigo, farinha de trigo integral, água, óleo de girassol, açucar, sal e fermento biológico.',
+    Aviso: 'Alérgicos e intolerantes: Todos os nossos preparos cotém glúten, portanto não são indicados para celíacos. Sem ingredientes de origem animal',
+    Tag: 'Sem ingredientes de origem animal',
+    Price: 'R$: 25,99',
+    SourceImage: 'https://cdn.neemo.com.br/uploads/item/photo/1011900/logo_kit.JPG',
+  },
+]
+
+const pizzas = [
+  {
+    id: 'Cheddar com Cebola Caramelizada',
+    Text: 'Cheddar artesanal com cebola caramelizada',
+    Acompanhamento: 'Acompanha maionese da casa',
+    Ingredientes: 'Igredientes massa: Farinha de trigo, farinha de trigo integral, água, óleo de girassol, açucar, sal e fermento biológico.',
+    Aviso: 'Alérgicos e intolerantes: Todos os nossos preparos cotém glúten, portanto não são indicados para celíacos. Sem ingredientes de origem animal',
+    Tag: 'Sem ingredientes de origem animal',
+    Price: 'R$: 25,99',
+    SourceImage: 'https://cdn.neemo.com.br/uploads/item/photo/1011900/logo_kit.JPG',
+  },
+]
+
+export default (esfirras, pizzas);
+
--- src/component/Cardapio.jsx
+++ src/component/Cardapio.jsx
@@ -4,10 +4,65 @@ class Cardapio extends React.Component {
   render() {
     return(
       <section>
+          <h2>Esfirras</h2>
         <div>
           <img></img>
-          <p>Esfirra de Soja</p>
+          <p>Maionese adicional 40g</p>
+          <p>Maionese da Padoca, feita sem ingredientes de origem animal, sem conservantes e sem industrializados.</p>
+          <p>R$ 2.00</p>
+        </div>
+        <div>
+          <img></img>
+          <p>Kit 6 Esfirras</p>
+          <p>Escolha os sabores</p>
+          <p>Kit de 6 esfirras com sabores variados. Aqui você pode escolher esse delicioso Kit com recheios variados. Acompanha maionese da Padoca.</p>
           <p>R$ 6.00</p>
+        </div>
+        <div>
+          <img></img>
+          <p>Esfirra Proteína de Soja</p>
+          <p>Proteína de soja defumada bem temperada e azeitona.</p>
+          <p>Acompanha maionese da casa</p>
+          <p>
+            Igredientes massa: Farinha de trigo, farinha de trigo integral, água, óleo de girassol, açucar, sal e fermento biológico.
+          </p>
+          <p>
+            Alérgicos e intolerantes: Todos os nossos preparos cotém glúten, portanto não são indicados para celíacos.
+          </p>
+          <p>
+            Sem ingredientes de origem animal</p>
+          <p>R$ 6.00</p>
+        </div>
+        <div>
+          <img></img>
+          <p>Esfirra de Marguerita</p>
+          <p>Queijo de castanha de caju, tomate fresco, orégano e manjericão fresco</p>
+          <p>R$ 6.00</p>
+        </div>
+        <div>
+          <img></img>
+          <p>Esfirra Cheddar com Cebola Caramelizada</p>
+          <p>Esfirra Proteína de Soja</p>
+          <p>R$ 6.00</p>
+        </div>
+        <h2>Pizzas</h2>
+        <div>
+          <img></img>
+          <p>Esfirra de Champignon</p>
+          <p>Esfirra Proteína de Soja</p>
+          <p>R$ 6.00</p>
+        </div>
+        <div>
+          <img></img>
+          <p>Esfirra de Marguerita</p>
+          <p>Esfirra Proteína de Soja</p>
+          <p>R$ 6.00</p>
+        </div>
+        <div>
+          <img></img>
+          <p>Esfirra de Abobrinha</p>
+          <p>Esfirra Proteína de Soja</p>
+          <p>R$ 30.00</p>
         </div>
       </section>
     )
